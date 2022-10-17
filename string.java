a7b6c769ddb8cbdd732180d4d875d7d7f62a5a1d
Smruti <99034389+SmrutiPatil@users.noreply.github.com>
2022-10-17 13:44:12 +0530

    JAVA files

---
+++ string.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class string{
+    public static main(String [] str){
+        String s1 = "apple";
+        String s2 = "banana";
+        String s3 = "apple";
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+    }
+}
